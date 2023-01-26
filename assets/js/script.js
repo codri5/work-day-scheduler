c9e2f8173ecb1aca6331fd19cc99cc7ccd71b7b8
codri5 <97296166+codri5@users.noreply.github.com>
2023-01-26 22:43:25 +0000

    add color code function and submit buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,26 @@ let month = moment().format('MMMM')
 let day = moment().format('Do')
 
 date.html(`${weekday}, ${month} ${day}`);
+
+// color code blocks based on current time
+let colorCode = $('.color-code');
+let currentTime = parseInt(moment().format('H'));
+
+colorCode.each(function() {
+    let hour = parseInt($(this).prop('id'));
+    if (hour > currentTime && hour < currentTime + 6 ) { $(this).css('background-color','lightgrey'); } 
+    else if (hour < currentTime && hour > currentTime - 6) { $(this).css('background-color','green'); } 
+    else if (hour === currentTime) { $(this).css('background-color', 'red'); } 
+    else { $(this).css('background-color','lightpink'); }
+});
+
+// create submit buttons
+$(document).ready(function() {
+    $('.submit').append(
+        $(document.createElement('button')).prop({
+            type: 'button',
+            class: 'btn-block'
+        })
+    );
+    $('button').append('<i class="fas fa-save style"></i>');
+});
